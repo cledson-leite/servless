76c8a345f166af269d8def11cf70848f6e2e0a10
cledson leite <40147313+cledson-leite@users.noreply.github.com>
2025-11-04 15:33:17 -0300

    criar tabela de eventos como primary key composta

---
+++ lib/eventsDynamoDB-stack.ts
@@ -0,0 +1,20 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+
+export class EventsDynamoDBStack extends cdk.Stack {
+  readonly table: dynamodb.Table;
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.table = new dynamodb.Table(this, 'EventsDymanoIdentifier', {
+      partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
+      timeToLiveAttribute: 'ttl',
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      readCapacity: 1,
+      writeCapacity: 1,
+    });
+}
+}
